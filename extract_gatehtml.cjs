2bf1a3dfc38eda89179bd35679609b47aa669ff0
martin <martincajurao@gmail.com>
2026-09-10 09:21:38 +0800

    gps

--- extract_gatehtml.cjs
+++
@@ -1,6 +0,0 @@
-/* location-gate HTML markup */
-id="location-gate" class="loc-gate hidden" role="dialog" aria-modal="true" aria-labelledby="loc-gate-title">
-      <div class="loc-gate-card">
-        <div class="loc-gate-handle"></div>
-        <div class="loc-gate-icon">📍</div>
-/* END */
